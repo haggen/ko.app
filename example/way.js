2bf524e8b540b250fa10d1ec6b3fdd83755474e3
Arthur Corenzan <arthur@corenzan.com>
2012-08-24 12:16:30 -0300

    update version of wayjs

--- example/way.js
+++ example/way.js
@@ -1,85 +1,8 @@
 /*
- * WayJS v0.1.0 2012-08-21 10:58:17 -0300
+ * WayJS v0.2.0 2012-08-23 22:44:15 -0300
  * by Arthur Corenzan <arthur@corenzan.com>
  * licensed under http://creativecommons.org/licenses/by/3.0
  * more on http://haggen.github.com/wayjs
  */
-(function(undefined) {
-
-  'use strict';
-
-  var Way;
-
-  Way = function() {
-    this.routes = [];
-    this.hooks = {};
-    this.params = {};
-  };
-
-  Way.prototype = {
-
-    map: function(pattern, action) {
-      var route = {};
-
-      route.params = [];
-      route.action = action;
-      route.pattern = this.translate(pattern, route);
-
-      this.routes.push(route);
-    },
-
-    translate: function(pattern, route) {
-      var re;
-
-      // Escape forward slashes
-      re = pattern.replace(/\//g, '\\/');
-
-      // Fix optional groups
-      re = re.replace(/\)/g, function() {
-        return ')?';
-      });
-
-      // Translate named parameters
-      re = re.replace(/:(\w[\w\d]*)|\(/g, function(m, n) {
-        // console.log(m, n, m === '(' ? '_' : n, m === '(' ? '(' : '([^\\/]+?)');
-        route.params.push(m === '(' ? '_' : n);
-        return m === '(' ? '(' : '([^\\/]+?)';
-      });
-
-      // Translate splats
-      re = re.replace(/\*/g, function() {
-        route.params.push('splat');
-        return '(.+?)';
-      });
-
-      return new RegExp('^' + re + '$');
-    },
-
-    match: function(path) {
-      var way, route, match, i, j;
-
-      way = this;
-      way.params = [];
-
-      for(i = 0; i < way.routes.length; i++) {
-        route = way.routes[i];
-        match = path.match(route.pattern);
-
-        if(match) {
-          for(j = 1; j < match.length; j++) {
-            way.params[route.params[j - 1]] = match[j];
-          }
-
-          return route.action;
-        }
-      }
-    }
-  };
-
-  if(typeof window === 'undefined') {
-    module.exports = new Way();
-  } else {
-    window.way = new Way();
-  }
-
-})();
+(function(){var c;c=function(){this.routes=[]};c.prototype={map:function(d,b){var a={params:[]};a.action=b;a.pattern=this.translate(d,a);this.routes.push(a)},translate:function(d,b){var a;a=d.replace(/\//g,"\\/");a=a.replace(/\)/g,function(){return")?"});a=a.replace(/:(\w[\w\d]*)|\(/g,function(a,d){b.params.push("("===a?"_":d);return"("===a?"(":"([^\\/]+)"});a=a.replace(/\*/g,function(){b.params.push("splat");return"(.+)"});return RegExp("^"+a)},match:function(d){var b,a,c,g,f,e;c=[];for(f=0;f<this.routes.length;f++)if(b=
+this.routes[f],a={action:b.action,params:[]},g=d.match(b.pattern)){for(e=1;e<g.length;e++)a.params[b.params[e-1]]=g[e];c.push(a)}return c}};"undefined"===typeof window?module.exports=new c:window.way=new c})();
